d1f59ab55783925f823c8dfec3f954085ff809dc
Devon Carew <devoncarew@gmail.com>
2014-12-16 16:39:48 -0800

    get set up for a ci; add copyright

---
+++ test/all.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library dartpad_server.all_test;
+
+void main() {
+  // TODO: placeholder for tests
+
+}
