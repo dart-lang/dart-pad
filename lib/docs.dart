4f97d4e4d0a0446731171cb872aa403a01de617e
Devon Carew <devoncarew@gmail.com>
2014-12-19 14:12:19 -0800

    refactor file locations

--- lib/docs.dart
+++ lib/docs.dart
@@ -1,8 +1,11 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
 import 'dart:html';
 
-import 'event_bus.dart';
+import 'core/event_bus.dart';
 
 EventBus _bus = new EventBus();
 
--- test/services/common_test.dart
+++ test/services/common_test.dart
@@ -1,10 +1,10 @@
-// Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
-// All rights reserved. Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 library dartpad_ui.common_test;
 
-import 'package:dartpad_ui/common.dart';
+import 'package:dartpad_ui/services/common.dart';
 import 'package:unittest/unittest.dart';
 
 void defineTests() {
