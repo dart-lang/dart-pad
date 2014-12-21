f02a00c958bcad94f6ed4768b72f45816cfffb84
Devon Carew <devoncarew@gmail.com>
2014-12-21 13:35:47 -0800

    upgrade ace; UI tweaks
    Upgrade to the new version of ace, to capture new API. UI tweaks; push
    html and css on reconcile, and make their reconcile timers much faster.

--- lib/dartpad.dart
+++ lib/dartpad.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library liftoff;
+library dartpad;
 
 import 'services/analysis.dart';
 import 'services/compiler.dart';
