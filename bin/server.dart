7b5fa931013cb7762a389a5c640e602e52002188
Luke Church <lukechurch@google.com>
2014-12-24 18:21:15 +0000

    CC headers

--- bin/server.dart
+++ bin/server.dart
@@ -1,3 +1,8 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// To meet GAE needs this file must be called 'server.dart'.
 
 import 'package:dartpad_server/dartpad_gae.dart' as server;
 
