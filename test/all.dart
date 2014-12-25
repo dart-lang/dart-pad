73a95710f1168f909bce57b3d1e1c34adaf91f7f
Luke Church <lukechurch@google.com>
2014-12-25 19:16:30 +0000

    Addition of basic operational tests

--- test/all.dart
+++ test/all.dart
@@ -1,10 +1,10 @@
 // Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-
 library dartpad_server.all_test;
 
-void main() {
-  // TODO: placeholder for tests
+import 'gae_deployed_test.dart' as gaeTest_test;
 
+void main() {
+  gaeTest_test.defineTests();
 }
---
+++ test/gae_deployed_test.dart
@@ -0,0 +1,46 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library gaeDeployed_test;
+
+import 'package:unittest/unittest.dart';
+import 'dart:io' as io;
+import 'package:http/http.dart' as http;
+import 'package:dartpad_server/src/common.dart' as common;
+
+final String serverUrl = "https://liftoff-dev.appspot.com";
+
+void main() => defineTests();
+
+void defineTests() {
+  group('gae deployed tests', () {
+    test('analyze end point', analyzeTest);
+    test('compile end point', compileTest);
+  });
+}
+
+
+analyzeTest() {
+  final String url = '${serverUrl}/api/analyze';
+  Map headers = {'Content-Type': 'text/plain; charset=UTF-8'};
+
+  expect(
+    http.post(url, headers: headers, body: common.sampleCodeWeb).then((response) {
+      expect(response.statusCode, 200);
+      expect(response.body.trim(), "[]");
+      return true;
+  }), completion(equals(true)));
+}
+
+compileTest() {
+  final String url = '${serverUrl}/api/compile';
+  Map headers = {'Content-Type': 'text/plain; charset=UTF-8'};
+
+  expect(
+    http.post(url, headers: headers, body: common.sampleCodeWeb).then((response) {
+      expect(response.statusCode, 200);
+      expect(true, response.body.length > 100);
+      return true;
+  }), completion(equals(true)));
+}
