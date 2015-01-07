40603d486ad0b640c866e18caadbad8e28a285c3
Devon Carew <devoncarew@gmail.com>
2015-01-07 12:46:28 -0800

    update copyrights

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+library dartpad_ui.util;
+
 import 'dart:html';
 import 'dart:math' as math;
 
