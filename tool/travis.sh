a7ed7f5811b014ad166ce8dd624c8bb532fc9441
Devon Carew <devoncarew@gmail.com>
2015-01-28 10:06:59 -0800

    switch the the new dart travis config

--- tool/travis.sh
+++ tool/travis.sh
@@ -7,20 +7,6 @@
 # Fast fail the script on failures.
 set -e
 
-# Get the Dart SDK.
-DART_DIST=dartsdk-linux-x64-release.zip
-curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
-unzip $DART_DIST > /dev/null
-rm $DART_DIST
-export DART_SDK="$PWD/dart-sdk"
-export PATH="$DART_SDK/bin:$PATH"
-
-# Display installed versions.
-dart --version
-
-# Get our packages.
-pub get
-
 # Verify that the libraries are error free.
 dartanalyzer --fatal-warnings \
   lib/dartpad.dart \
