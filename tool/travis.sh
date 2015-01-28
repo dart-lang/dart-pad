b97e71e6db35135c4d75e212481527cf042d33a8
Devon Carew <devoncarew@gmail.com>
2015-01-28 10:07:53 -0800

    switch to the dart travis config

--- tool/travis.sh
+++ tool/travis.sh
@@ -4,23 +4,6 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
-# Fast fail the script on failures.
-set -e
-
-# Get the Dart SDK.
-DART_DIST=dartsdk-linux-x64-release.zip
-curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
-unzip $DART_DIST > /dev/null
-rm $DART_DIST
-export DART_SDK="$PWD/dart-sdk"
-export PATH="$DART_SDK/bin:$PATH"
-
-# Display installed versions.
-dart --version
-
-# Get our packages.
-pub get
-
 # Verify that the libraries are error free.
 dartanalyzer --fatal-warnings \
   bin/dartpad_server.dart \
