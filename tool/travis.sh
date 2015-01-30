47fee077e5c5e6b5ec2499d8090a37f3cf975960
Devon Carew <devoncarew@gmail.com>
2015-01-29 21:55:25 -0800

    fix travis script

--- tool/travis.sh
+++ tool/travis.sh
@@ -4,6 +4,9 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
+# Fast fail the script on failures.
+set -e
+
 # Verify that the libraries are error free.
 dartanalyzer --fatal-warnings \
   bin/dartpad_server.dart \
