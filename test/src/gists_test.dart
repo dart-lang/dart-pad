c5a2d9fbf58295a2fe1ca5e7ee6810118e081d86
Devon Carew <devoncarew@gmail.com>
2015-02-19 14:22:50 -0800

    fix build

--- test/src/gists_test.dart
+++ test/src/gists_test.dart
@@ -1,10 +1,8 @@
-// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
 library dart_pad.gists_test;
-
-import 'dart:async';
 
 import 'package:dart_pad/src/gists.dart';
 import 'package:unittest/unittest.dart';
