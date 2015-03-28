bc3b01515878e2b4882d5aee9d7933efc4c9e648
Kasper Peulen <kasperpeulen@gmail.com>
2015-03-29 00:39:33 +0100

    added dartdoc for Editor.cursorCoords

--- lib/editing/editor.dart
+++ lib/editing/editor.dart
@@ -39,6 +39,11 @@ abstract class Editor {
   String get theme;
   set theme(String str);
 
+  /**
+   * Returns the cursor coordinates in pixels.
+   * cursorCoords.x corresponds to left and cursorCoords.y corresponds to top.
+   * Only implemented for codemirror, returns null for ace editor and comid.
+   */
   Point get cursorCoords;
 
   void resize();
