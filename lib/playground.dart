8406d8e96e60c0374e51ba03dde72a433616eab4
Kasper Peulen <kasperpeulen@gmail.com>
2015-03-31 00:58:02 +0200

    parameter popup behind dev flag

--- lib/playground.dart
+++ lib/playground.dart
@@ -49,6 +49,7 @@ class Playground {
   DivElement get _docPanel => querySelector('#documentation');
   bool get _isCompletionActive => editor.completionActive;
   bool get _isDocPanelOpen => querySelector("#doctab").attributes.containsKey('selected');
+  bool get _parPopupActive => querySelector(".parameter-hints") != null;
 
   DButton runbutton;
   DOverlay overlay;
@@ -199,23 +200,45 @@ class Playground {
       _toggleDocTab();
       _handleHelp();
     });
+
+    document.onKeyDown.listen((e) {
+      if (e.keyCode == KeyCode.ENTER) {
+        // TODO: this should probably only be fired when the completion popup was active
+        // TODO: but there doesn't seem a way to find this out
+        // TODO: maybe adding _loopParameterInfo to hintApplier in editor_codemirror.dart ?
+        _lookupParameterInfo();
+      }
+    });
+
     document.onKeyUp.listen((e) {
+
+      //documentation
+      if (_isCompletionActive || cursorKeys.contains(e.keyCode)) {
+        _handleHelp();
+      }
+
+      //autocompletion
       if (options.getValueBool('autopopup_code_completion')) {
         RegExp exp = new RegExp(r"[a-zA-Z]");
-        // TODO: _isCompletionActive won't work correct
-        // TODO: which causes some issues
-        // TODO: will be fixed when we use the latest codemirror.js version
         if (!_isCompletionActive && exp.hasMatch(
             new String.fromCharCode(e.keyCode)) || e.keyCode == KeyCode.PERIOD) {
           editor.execCommand("autocomplete");
         }
       }
-      if (_isCompletionActive || cursorKeys.contains(e.keyCode)) {
-        _handleHelp();
-      }
-    });
-    document.onClick.listen((e) => _handleHelp());
 
+      //parameter popup
+      if (e.keyCode == KeyCode.ESC) {
+        document.body.children.remove(querySelector(".parameter-hints"));
+        return;
+      }
+      _lookupParameterInfo(e);
+    });
+    
+    document.onClick.listen((e) {
+      _handleHelp();
+      _lookupParameterInfo();
+    });
+    
     querySelector("#doctab").onClick.listen((e) => _toggleDocTab());
     querySelector("#consoletab").onClick.listen((e) => _toggleConsoleTab());
 
@@ -242,6 +265,7 @@ class Playground {
 
     // Set up development options.
     options.registerOption('autopopup_code_completion', 'false');
+    options.registerOption('parameter_popup', 'false');
 
     _finishedInit();
   }
@@ -284,12 +308,12 @@ class Playground {
     _outputpanel.style.display = "none";
     querySelector("#consoletab").attributes.remove('selected');
 
-    _docPanel..style.display = "block";
+    _docPanel.style.display = "block";
     querySelector("#doctab").setAttribute('selected','');
   }
 
   void _toggleConsoleTab() {
-    _docPanel..style.display = "none";
+    _docPanel.style.display = "none";
     querySelector("#doctab").attributes.remove('selected');
 
     _outputpanel.style.display = "block";
@@ -415,6 +439,162 @@ ${result.info['libraryName'] != null ? "**Library:** ${result.info['libraryName'
 ''', inlineSyntaxes: [ new InlineBracketsColon(), new InlineBrackets()]), validator: _htmlValidator);
         }
       });
+    }
+  }
+
+  ///Returns null if the offset is not contained in parenthesis.
+  ///Otherwise it will return information about the parameters.
+  ///For example, if the source is `substring(1, <caret>)`, it will return
+  ///`{openingParenIndex: 9, parameterIndex: 1}`.
+  Map<String,int> _parameterInfo(String source, int offset) {
+    int parameterIndex = 0;
+    int openingParenIndex;
+    int nesting = 0;
+
+    while (openingParenIndex == null && offset > 0) {
+      offset += -1;
+      if (nesting == 0) {
+        switch (source[offset]) {
+          case "(":
+            openingParenIndex = offset;
+            break;
+          case ",":
+            parameterIndex += 1;
+            break;
+          case ";":
+            return null;
+          case ")":
+            nesting += 1;
+            break;
+        }
+      } else {
+        switch(source[offset]) {
+          case "(":
+            nesting += -1;
+            break;
+          case ")":
+            nesting += 1;
+            break;
+        }
+      }
+    }
+    return openingParenIndex == null ? null : {
+      "openingParenIndex" : openingParenIndex,
+      "parameterIndex" : parameterIndex
+    };
+  }
+
+  void _lookupParameterInfo([KeyboardEvent e]) {
+
+    if(!options.getValueBool("parameter_popup")) {
+      return;
+    }
+
+    if (context.focusedEditor != 'dart' || !editor.hasFocus) {
+      document.body.children.remove(querySelector(".parameter-hints"));
+      return;
+    }
+
+    if (e != null && !_parPopupActive) {
+      //so if we have a keyboardevent (not a click)
+      const List navKeys = const[KeyCode.COMMA, KeyCode.NINE, KeyCode.ZERO, KeyCode.LEFT, KeyCode.RIGHT, KeyCode.UP, KeyCode.DOWN];
+      if (!navKeys.contains(e.keyCode)) {
+        return;
+      }
+    }
+
+    int offset = editor.document.indexFromPos(editor.document.cursor);
+    String source = _context.dartSource;
+    Map<String, int> parInfo = _parameterInfo(source, offset);
+
+    if (parInfo == null) {
+      document.body.children.remove(querySelector(".parameter-hints"));
+      return;
+    }
+
+    int openingParenIndex = parInfo["openingParenIndex"], parameterIndex = parInfo["parameterIndex"];
+    offset = openingParenIndex - 1;
+
+    //we then request documentation info of what is before that parenthesis
+    SourceRequest input = new SourceRequest()
+      ..source = source
+      ..offset = offset;
+
+    dartServices.document(input).timeout(serviceCallTimeout)
+    .then((DocumentResponse result) {
+      if (!result.info.containsKey("parameters")) {
+        document.body.children.remove(querySelector(".parameter-hints"));
+        return;
+      }
+      List parameterInfo = result.info["parameters"] as List;
+      String outputString = "";
+      if (parameterInfo.length == 0) {
+        outputString += "<code>&lt;no parameters&gt;</code>";
+      } else if (parameterInfo.length < parameterIndex + 1) {
+        outputString += "<code>too many parameters listed</code>";
+      } else {
+        outputString += "<code>";
+        var sanitizer = const HtmlEscape();
+
+        for (int i = 0; i < parameterInfo.length; i++) {
+          if (i == parameterIndex) {
+            outputString += '<em>${parameterInfo[i]}</em>';
+          } else {
+            outputString += '${sanitizer.convert(parameterInfo[i])}';
+          }
+          if (i != parameterInfo.length - 1) {
+            outputString += ", ";
+          }
+        }
+        outputString += "</code>";
+      }
+
+      //check again if cursor is in parameter position
+      offset = editor.document.indexFromPos(editor.document.cursor);
+      source = _context.dartSource;
+      parInfo = _parameterInfo(source, offset);
+      if (parInfo == null) {
+        document.body.children.remove(querySelector(".parameter-hints"));
+        return;
+      }
+      _showParameterPopup(outputString);
+    });
+  }
+
+  void _showParameterPopup(String string) {
+    DivElement editorDiv = querySelector("#editpanel .CodeMirror");
+    var lineHeight = editorDiv.getComputedStyle().getPropertyValue('line-height');
+    lineHeight = int.parse(lineHeight.substring(0, lineHeight.indexOf("px")));
+    //var charWidth = editorDiv.getComputedStyle().getPropertyValue('letter-spacing');
+    int charWidth = 8;
+
+    Point cursorCoords = editor.cursorCoords;
+
+    if (_parPopupActive) {
+      var parameterHint = querySelector(".parameter-hint");
+      parameterHint.innerHtml = string;
+
+      //update popup position
+      int newLeft = math.max(cursorCoords.x - (parameterHint.text.length * charWidth ~/ 2),0);
+
+      var parameterPopup = querySelector(".parameter-hints")
+        ..style.top = "${cursorCoords.y - lineHeight - 5}px";
+      var oldLeft = parameterPopup.style.left;
+      oldLeft = int.parse(oldLeft.substring(0,oldLeft.indexOf("px")));
+      if ((newLeft - oldLeft).abs() > 50) {
+        parameterPopup.style.left = "${newLeft}px";
+      }
+    } else {
+      var parameterHint = new SpanElement()
+        ..innerHtml = string
+        ..classes.add("parameter-hint");
+      int left = math.max(cursorCoords.x - (parameterHint.text.length * charWidth ~/ 2), 0);
+      var parameterPopup = new DivElement()
+        ..classes.add("parameter-hints")
+        ..style.left = "${left}px"
+        ..style.top = "${cursorCoords.y - lineHeight - 5}px";
+      parameterPopup.append(parameterHint);
+      document.body.append(parameterPopup);
     }
   }
 
