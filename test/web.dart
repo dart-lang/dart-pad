d5760c5a4f20069a4c9b5bcb98c6858857c8e70a
Devon Carew <devoncarew@gmail.com>
2015-04-08 09:49:03 -0700

    add a test for the MDN links

--- test/web.dart
+++ test/web.dart
@@ -9,6 +9,7 @@ import 'package:grinder/src/webtest.dart';
 import 'core/dependencies_test.dart' as dependencies_test;
 import 'core/event_bus_test.dart' as event_bus_test;
 import 'core/keys_test.dart' as keys_test;
+import 'documentation_test.dart' as documentation_test;
 import 'services/common_test.dart' as common_test;
 import 'sharing/gists_test.dart' as gists_test;
 
@@ -18,8 +19,9 @@ void main() {
 
   // Define the tests.
   dependencies_test.defineTests();
-  common_test.defineTests();
   event_bus_test.defineTests();
   keys_test.defineTests();
+  documentation_test.defineTests();
+  common_test.defineTests();
   gists_test.defineTests();
 }
