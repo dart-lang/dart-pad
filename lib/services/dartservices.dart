a82fab3aa4820e224e629e5e1ca4990b6f3640e6
Devon Carew <devoncarew@gmail.com>
2015-05-15 10:46:55 -0700

    comment out unused imports

--- lib/services/dartservices.dart
+++ lib/services/dartservices.dart
@@ -3,12 +3,12 @@
 library services.dartservices.v1;
 
 import 'dart:core' as core;
-import 'dart:collection' as collection;
+//import 'dart:collection' as collection;
 import 'dart:async' as async;
 import 'dart:convert' as convert;
 
 import 'package:_discoveryapis_commons/_discoveryapis_commons.dart' as commons;
-import 'package:crypto/crypto.dart' as crypto;
+//import 'package:crypto/crypto.dart' as crypto;
 import 'package:http/http.dart' as http;
 
 export 'package:_discoveryapis_commons/_discoveryapis_commons.dart' show
