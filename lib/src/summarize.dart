b3a121030f3cd3df8b8adb7a2f4acc5cbdbe48a8
ghe <ghe@ghe-macbookpro.roam.corp.google.com>
2015-05-26 13:03:27 -0700

    Summarizer initial creation

---
+++ lib/src/summarize.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library dart_pad.summarize;
+
+class Summarizer {
+  
+  String summarize(String input) {
+//    String size;
+//    String packages;
+//    String libraries;
+//    String features;
+//    String complexity;
+    return "foo";
+    }
+}
+
---
+++ test/src/summarize_test.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library dartpad.summarize_test;
+
+import 'package:dart_pad/src/summarize.dart';
+import 'package:unittest/unittest.dart';
+
+void defineTests() {
+  group('summarization', () {
+    //Verify that summarizer returns non-null input
+    test('non-null', () {
+      Summarizer summer = new Summarizer();
+      expect(summer.summarize(""), isNot(equals(null)));
+    });
+  });
+}
