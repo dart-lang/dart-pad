0e60b1ee2f0b3e83d1316f234f72151081e43478
ghe <ghe@google.com>
2015-05-28 15:13:23 -0700

    Added general categories

--- lib/src/summarize.dart
+++ lib/src/summarize.dart
@@ -4,6 +4,8 @@
 
 library dart_pad.summarize;
 
+import 'package:crypto/crypto.dart';
+
 import '../services/dartservices.dart';
 
 /// Instances of this class take string input of dart code as well as an analysis result,
@@ -12,14 +14,66 @@ class Summarizer {
   _SummarizeToken storage;
 
   bool resultsPresent;
+  MD5 encryptor;
+  static Map<String, List<int>> cuttoffs = {
+    'size': [8, 30, 100],
+    'errors': [1,5,10]
+  };
+  static Map<String, List<String>> categories = {
+    /// This [size] [codeQuantifier] contains [error] errors and warnings.
+    'size-2': ['gigantic', 'Jupiterian sized', 'immense', 
+      'massive', 'enormous', 'huge', 'epic', 'humongous'],
+    'size-1': ['decently sized', 'exceptional', 'awesome', 
+      'amazing', 'visionary', 'legendary'],
+    'size-0': ['itty-bitty', 'miniature', 'tiny', 'pint-sized'],
+    'compiledQuantifier': ['DART program', 'pad', ''],
+    'failedQuantifier': ['assemblage of characters', 'series of strings', 'grouping of letters'],
+    'errorCount-2':['many', 'a motherload of', 'copious amounts of', 'unholy quantities of'],
+    'errorCount-1':['some', 'a few', 'sparse amounts of', 'very few instances of'],
+    'errorCount-0':['zero', 'no', 'a void of', '0.0000', '0'],
+  };
   
   Summarizer(String input, [AnalysisResults analysis]) {
     if (input == null) throw new ArgumentError("Input can't be null.");
     resultsPresent = !(analysis == null);
+    encryptor = new MD5();
+    encryptor.add(input.codeUnits);
     storage = new _SummarizeToken(input, analysis);
   }
   
+  String _wordSelector(String category) {
+    if (categories.containsKey(category)) {
+      
+    }
+    else {
+      
+    }
+  }
+  
   String returnAsSimpleSummary() {
+      if (resultsPresent) {
+        String summary = "Summary:";
+        summary +='This ${storage.linesCode} lines of code.\n';
+        if (storage.errorPresent) summary += 'Errors are present.\n';
+        if (storage.errorPresent) summary += 'Warnings are present.\n';
+        summary += '${storage.features}\n\n';
+        summary += '${storage.packageCount} Package Imports: ${storage.packageImports}.\n';
+        summary += '${storage.resolvedCount} Resolved Imports: ${storage.resolvedImports}.\n';
+        summary += 'List of ${storage.errorCount} Errors:\n';
+        for (AnalysisIssue issue in storage.errors) {
+          summary += '- ${_condenseIssue(issue)}\n';
+        }
+        summary += "```";
+        return summary;
+      }
+      else {
+        String summary = "``` \n-- Summary (Under Development) --\n";
+        summary +='${storage.linesCode} lines of code.\n';
+        return summary;
+      }
+    }
+  
+  String returnAsVerboseSummary() {
     if (resultsPresent) {
       String summary = "Summary:";
       summary +='There are ${storage.linesCode} lines of code.\n';
