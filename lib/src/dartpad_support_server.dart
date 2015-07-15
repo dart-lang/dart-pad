f9196e7084e3bde5dd1ea336d8ca4319a6773dde
ghe <ghe@google.com>
2015-07-15 11:03:25 -0700

    String to blob, sha1

--- lib/src/dartpad_support_server.dart
+++ lib/src/dartpad_support_server.dart
@@ -10,6 +10,7 @@ import 'package:appengine/appengine.dart' as ae;
 import 'package:gcloud/db.dart' as db;
 import 'package:crypto/crypto.dart' as crypto;
 import 'dart:convert' as convert;
+import 'dart:io' as io;
 import 'package:logging/logging.dart';
 
 final Logger _logger = new Logger('dartpad_support_server');
@@ -24,7 +25,7 @@ class FileRelayServer {
   }
   
   @ApiMethod(method:'POST', path:'export') 
-  Future<KeyContainer> export(DataSaveObject data){
+  Future<KeyContainer> export(PadSaveObject data){
     GaeExportRecord record = new GaeExportRecord.FromDSO(data);
     sha1SetUUID(record);
     db.dbService.commit(inserts: [record])
@@ -37,14 +38,14 @@ class FileRelayServer {
   }
   
   @ApiMethod(method:'DELETE', path:'pullExportData')
-  Future<DataSaveObject> pullExportContent({String key}) async {
+  Future<PadSaveObject> pullExportContent({String key}) async {
     var database = ae.context.services.db;
     var query = database.query(GaeExportRecord)
         ..filter('UUID =', key);
     List result = await query.run().toList();
     if (result.isEmpty) {
       _logger.severe("Export with key ${key} could not be found.");
-      return new Future.value(new DataSaveObject());
+      return new Future.value(new PadSaveObject());
     }
     GaeExportRecord record = result.first;
     database.commit(deletes: [record.key]).catchError((e) {
@@ -52,7 +53,7 @@ class FileRelayServer {
       throw(e);
     });
     _logger.info("PERF: Deleted Export with ID ${record.UUID}");
-    return new Future.value(new DataSaveObject.FromData(record.dart, record.html, record.css));
+    return new Future.value(new PadSaveObject.FromRecordSource(record));
   }
 }
 
@@ -61,47 +62,68 @@ class FileRelayServer {
  */
 @db.Kind()
 class GaeExportRecord extends db.Model {
-  @db.StringProperty()
-  String dart;
+  @db.BlobProperty()
+  List<int> dart;
+  
+  @db.IntProperty()
+  int epochTime;
 
-  @db.StringProperty()
-  String html;
+  @db.BlobProperty()
+  List<int> html;
 
-  @db.StringProperty()
-  String css;
+  @db.BlobProperty()
+  List<int> css;
 
   @db.StringProperty()
   String UUID;
   
-  GaeExportRecord();
+  GaeExportRecord() {
+    this.epochTime = new DateTime.now().millisecondsSinceEpoch;
+  }
 
+  List<int> GZIPencode(String input) => io.GZIP.encode(convert.UTF8.encode(input));
+  
   GaeExportRecord.FromData(String dart, String html, String css, {String UUID}) {
-    this.dart = dart;
-    this.html = html;
-    this.css = css;
+    this.dart = GZIPencode(dart);
+    this.html = GZIPencode(html);
+    this.css = GZIPencode(css);;
     this.UUID = UUID;
+    this.epochTime = new DateTime.now().millisecondsSinceEpoch;
   }
   
-  GaeExportRecord.FromDSO(DataSaveObject dso) {
-    this.dart = dso.dart;
-    this.html = dso.html;
-    this.css = dso.css;
-    this.UUID = dso.UUID;
+  GaeExportRecord.FromDSO(PadSaveObject pso) {
+    this.dart = GZIPencode(pso.dart);
+    this.html = GZIPencode(pso.html);
+    this.css = GZIPencode(pso.css);
+    this.UUID = pso.UUID;
+    this.epochTime = new DateTime.now().millisecondsSinceEpoch;
   }
+  
+  String GZIPdecode(List<int> input) => convert.UTF8.decode(io.GZIP.decode(input));
+  String get getDart => GZIPdecode(this.dart);
+  String get getHtml => GZIPdecode(this.html);
+  String get getCss => GZIPdecode(this.css);
 }
 
-class DataSaveObject {
+class PadSaveObject {
   String dart;
   String html;
   String css;
   String UUID;
-  DataSaveObject();
-
-  DataSaveObject.FromData(String dart, String html, String css, {String UUID}) {
+  PadSaveObject();
+  
+  PadSaveObject.FromData(String dart, String html, String css, {String UUID}) {
     this.dart = dart;
     this.html = html;
     this.css = css;
     this.UUID = UUID;
+  }
+  
+  PadSaveObject.FromRecordSource(GaeExportRecord record) {
+    this.dart =  record.getDart;
+    this.html =  record.getHtml;
+    this.css =  record.getCss;
+    this.UUID = record.UUID;
   }
 }
 
@@ -113,10 +135,14 @@ class KeyContainer {
   }
 }
 
-// SHA1 set the id
-void sha1SetUUID(GaeExportRecord record) {
+List<int> computeSHA1(GaeExportRecord record) {
   crypto.SHA1 sha1 = new crypto.SHA1();
   convert.Utf8Encoder utf8 = new convert.Utf8Encoder();
-  sha1.add(utf8.convert('blob  \n ${record.html} ${record.css} ${record.dart}'));
-  record.UUID = crypto.CryptoUtils.bytesToHex(sha1.close());
+  sha1.add(utf8.convert("blob  'n ${record.getDart} ${record.getHtml} ${record.getCss}"));
+  return sha1.close();
+}
+
+// SHA1 set the id
+void sha1SetUUID(GaeExportRecord record) {
+  record.UUID = crypto.CryptoUtils.bytesToHex(computeSHA1(record));
 }
