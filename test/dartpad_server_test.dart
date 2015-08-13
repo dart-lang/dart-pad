1e4071020825e116b63dadf6d5c8ffb28ef0cc6c
ghe <ghe@google.com>
2015-08-13 16:50:36 -0700

    Unit test updates

--- test/dartpad_server_test.dart
+++ test/dartpad_server_test.dart
@@ -94,151 +94,16 @@ void defineTests() {
 
   group('GistMapping', () {
 
-    test('analyze negative-test noSource', () async {
-       var json = {};
-       var response = await _sendPostRequest('_dartpadsupportservices/v1/analyze', json);
-       expect(response.status, 400);
-    });
-
-    test('compile', () async {
-      var json = {'source': sampleCode};
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/compile', json);
-      expect(response.status, 200);
-      var data = await response.body.first;
-      expect(JSON.decode(UTF8.decode(data)), isNotEmpty);
-    });
-
-    test('compile error', () async {
-      var json = {'source': sampleCodeError};
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/compile', json);
-      expect(response.status, 400);
-      var data = JSON.decode(UTF8.decode(await response.body.first));
-      expect(data, isNotEmpty);
-      expect(data['error']['message'], contains('[error on line 2] Expected'));
-    });
-
-    test('compile negative-test noSource', () async {
-        var json = {};
-        var response = await _sendPostRequest('_dartpadsupportservices/v1/compile', json);
-        expect(response.status, 400);
-     });
-
-    test('complete', () async {
-      var json = {'source': 'void main() {print("foo");}', 'offset': 1};
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/complete', json);
+    test('ID request', () async {
+      var response = await _sendGetRequest('_dartpadsupportservices/v1/getUnusedMappingId');
       expect(response.status, 200);
       var data = JSON.decode(UTF8.decode(await response.body.first));
-      expect(data, isNotEmpty);
-    });
+      expect(data['uuid'] != null, true);
+      var responseTwo = await _sendGetRequest('_dartpadsupportservices/v1/getUnusedMappingId');
+      expect(responseTwo.status, 200);
+      var dataTwo = JSON.decode(UTF8.decode(await responseTwo.body.first));
+      expect(data['uuid'] == dataTwo['uuid'], false);
 
-    test('complete no data', () async {
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/complete', {});
-      expect(response.status, 400);
-    });
-
-    test('complete param missing', () async {
-      var json = {'offset': 1};
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/complete', json);
-      expect(response.status, 400);
-    });
-
-    test('complete param missing 2', () async {
-      var json = {'source': 'void main() {print("foo");}'};
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/complete', json);
-      expect(response.status, 400);
-      var data = JSON.decode(UTF8.decode(await response.body.first));
-      expect(data['error']['message'], 'Missing parameter: \'offset\'');
-    });
-
-    test('document', () async {
-      var json = {'source': 'void main() {print("foo");}', 'offset': 17};
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/document', json);
-      expect(response.status, 200);
-      var data = JSON.decode(UTF8.decode(await response.body.first));
-      expect(data, isNotEmpty);
-    });
-
-    test('document little data', () async {
-      var json = {'source': 'void main() {print("foo");}', 'offset': 2};
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/document', json);
-      expect(response.status, 200);
-      var data = JSON.decode(UTF8.decode(await response.body.first));
-      expect(data, {"info": {"staticType": "void"}});
-    });
-
-    test('document no data', () async {
-      var json = {'source': 'void main() {print("foo");}', 'offset': 12};
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/document', json);
-      expect(response.status, 200);
-      var data = JSON.decode(UTF8.decode(await response.body.first));
-      expect(data, {"info": {}});
-    });
-
-    test('document negative-test noSource', () async {
-      var json = { 'offset': 12 };
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/document', json);
-      expect(response.status, 400);
-    });
-
-    test('document negative-test noOffset', () async {
-      var json = {'source': 'void main() {print("foo");}' };
-      var response = await _sendPostRequest('_dartpadsupportservices/v1/document', json);
-      expect(response.status, 400);
-    });
-
-    test('counter test', () async {
-      var response =
-        await _sendGetRequest('dartservices/v1/counter', "name=Analyses");
-      var data = JSON.decode(UTF8.decode(await response.body.first));
-      expect(response.status, 200);
-      expect(data['count'], 0);
-
-      // Do an Analysis.
-      var json = {'source': sampleCode};
-      response = await _sendPostRequest('dartservices/v1/analyze', json);
-
-      response =
-        await _sendGetRequest('dartservices/v1/counter', "name=Analyses");
-      data = JSON.decode(UTF8.decode(await response.body.first));
-      expect(response.status, 200);
-      expect(data['count'], 1);
-    });
-
-    test('format', () async {
-      var json = {'source': preFormattedCode};
-      var response = await _sendPostRequest('dartservices/v1/format', json);
-      expect(response.status, 200);
-      var data = JSON.decode(UTF8.decode(await response.body.first));
-      expect(data["newString"], postFormattedCode);
-    });
-
-    test('format position', () async {
-      var json = {'source': preFormattedCode, 'offset': 21};
-      var response = await _sendPostRequest('dartservices/v1/format', json);
-      expect(response.status, 200);
-      var data = JSON.decode(UTF8.decode(await response.body.first));
-      expect(data["newString"], postFormattedCode);
-      expect(data["offset"], 24);
-    });
-
-    test('fix', () async {
-      var json = {'source': quickFixesCode, 'offset': 10};
-      var response = await _sendPostRequest('dartservices/v1/fixes', json);
-      expect(response.status, 200);
-      var data = JSON.decode(UTF8.decode(await response.body.first));
-      var fixes = data['fixes'];
-      expect(fixes.length, 1);
-      var problemAndFix = fixes[0];
-      expect(problemAndFix['problemMessage'], isNotNull);
-    });
-
-    test('version', () async {
-      var response = await _sendGetRequest('dartservices/v1/version');
-      expect(response.status, 200);
-      var data = JSON.decode(UTF8.decode(await response.body.first));
-      print(data);
-      expect(data['sdkVersion'], isNotNull);
-      expect(data['runtimeVersion'], isNotNull);
     });
   });
 }
