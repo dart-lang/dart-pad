066ecd2e8f74f89308902593e38b7e48e906a9ca
Luke Church <luke@church.name>
2017-05-18 16:08:51 -0700

    Disable vulcanize (#765)
    * Test disable vulcanize
    
    * Disable bower

--- tool/travis.sh
+++ tool/travis.sh
@@ -8,8 +8,8 @@
 set -e
 
 # Install the bower and vulcanize.
-npm install -g bower
-npm install -g vulcanize
+# npm install -g bower
+# npm install -g vulcanize
 
 # Run the analyze/test/build script.
 dart tool/grind.dart buildbot
