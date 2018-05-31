ba7625a63413fa95a32ab4f7d2e1cf13d084a110
Janice Collins <jcollins@google.com>
2018-05-31 12:10:26 -0700

    dart 2 changes for dart-pad

--- lib/services/common.dart
+++ lib/services/common.dart
@@ -5,7 +5,8 @@
 library dart_pad.common;
 
 final String serverURL = 'https://dart-services.appspot.com/';
-//final String serverURL = 'http://127.0.0.1:8080/';
+//final String serverURL = 'http://127.0.0.1:8082/';
+//final String serverURL = 'https://dart2-test-dot-dart-services.appspot.com/';
 
 final Duration serviceCallTimeout = new Duration(seconds: 10);
 final Duration shortServiceCallTimeout = new Duration(seconds: 4);
