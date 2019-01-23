66d9081bcee6ce1f79fe4740b3d586248091a3c0
Janice Collins <jcollins@google.com>
2019-01-23 14:26:24 -0800

    Tweak parameters per @kevmoo

--- tool/grind.dart
+++ tool/grind.dart
@@ -91,8 +91,7 @@ serveCustomBackend() {
 
 @Task('Build the `web/index.html` entrypoint')
 build() {
-  PubApp.global('webdev').activate();
-  PubApp.global('webdev').run(['build']);
+  PubApp.local('build_runner').run(['build', '-r', '-o', 'web:build']);
 
   FilePath mainFile = _buildDir.join('scripts/main.dart.js');
   log('${mainFile} compiled to ${_printSize(mainFile)}');
