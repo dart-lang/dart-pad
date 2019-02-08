7dcb34865c98297174879ad1d99b08948657740b
Andrew Brogdon <brogdon@gmail.com>
2019-02-08 10:18:47 -0800

    Fixing analyzer issue.

--- lib/experimental/new_embed.dart
+++ lib/experimental/new_embed.dart
@@ -2,14 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:logging/logging.dart';
-
 NewEmbed get playground => _playground;
 
 NewEmbed _playground;
-
-final _logger = Logger('dartpad');
-
 void init() {
   _playground = NewEmbed();
 }
