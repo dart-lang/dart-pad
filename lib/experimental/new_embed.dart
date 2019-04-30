090c72aebfd1cc026a259cb2cd11f197a0c4d0c9
John Ryan <ryjohn@google.com>
2019-04-30 09:24:11 -0700

    filter out paths to SDKs in stack traces (#1055)
    * filter out paths to SDKs in stack traces
    
    closes #1035
    
    * address review comments (#1055)
    
    Move filter function into existing files

--- lib/experimental/new_embed.dart
+++ lib/experimental/new_embed.dart
@@ -497,20 +497,20 @@ class TabView {
 }
 
 class ConsoleTabView extends TabView {
-  const ConsoleTabView(DElement element) : super(element);
+  ConsoleTabView(DElement element) : super(element);
 
   void clear() {
     element.text = '';
   }
 
   void appendMessage(String msg) {
-    final line = DivElement()..text = msg;
+    final line = DivElement()..text = filterCloudUrls(msg);
     element.add(line);
   }
 
   void appendError(String err) {
     final line = DivElement()
-      ..text = err
+      ..text = filterCloudUrls(err)
       ..classes.add('text-red');
     element.add(line);
   }
@@ -707,4 +707,16 @@ class NewEmbedContext {
     // TODO(DomesticMouse): implement with CodeMirror integration
     return false;
   }
+}
+
+final RegExp _flutterUrlExp =
+    RegExp(r'(https:[a-zA-Z0-9_=%&\/\-\?\.]+flutter_web\.js)(:\d+:\d+)');
+final RegExp _dartUrlExp =
+    RegExp(r'(https:[a-zA-Z0-9_=%&\/\-\?\.]+dart_sdk\.js)(:\d+:\d+)');
+
+String filterCloudUrls(String trace) {
+  return trace
+      .replaceAllMapped(
+          _flutterUrlExp, (m) => '[Flutter SDK Source]${m.group(2)}')
+      .replaceAllMapped(_dartUrlExp, (m) => '[Dart SDK Source]${m.group(2)}');
 }
