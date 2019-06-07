889a8e82d62e6b94fa344d300cdabb9ca0e47cd4
John Ryan <ryjohn@google.com>
2019-06-07 15:06:31 -0700

    use separate script entrypoints

---
+++ web/experimental/new_embed_html.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:dart_pad/experimental/new_embed.dart' as new_embed;
+import 'package:logging/logging.dart';
+
+void main() {
+  new_embed.init();
+
+  Logger.root.onRecord.listen(print);
+}
