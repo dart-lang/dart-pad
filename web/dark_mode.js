37d515a1d4e49452bdaf8274f9d4693effd2530f
John Ryan <ryjohn@google.com>
2019-08-13 12:26:56 -0700

    fix links in embed-dart.html

---
+++ web/dark_mode.js
@@ -0,0 +1,14 @@
+(function() {
+    var params = new URLSearchParams(window.location.search);
+    if (params.get('theme') == 'dark') {
+        document.write(
+            '<link rel="stylesheet" type="text/css" href="/packages/primer_css/primer_12.1.0_dark.css">' +
+            '<link rel="stylesheet" type="text/css" href="experimental/styles_dark.css">'
+        );
+    } else {
+        document.write(
+            '<link rel="stylesheet" type="text/css" href="/packages/primer_css/primer_12.1.0.css">' +
+            '<link rel="stylesheet" type="text/css" href="experimental/styles_light.css">'
+        )
+    }
+})();
