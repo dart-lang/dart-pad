0bc7393e206e656a1bf99bd1c967c58b1e09eb7b
Andrew Brogdon <RedBrogdon@users.noreply.github.com>
2019-08-19 11:08:06 -0700

    Update cursor after quick fix, using new data from dart-services (#1210)

--- lib/editing/editor.dart
+++ lib/editing/editor.dart
@@ -186,10 +186,25 @@ class Completion {
   /// This can be `null`.
   final int cursorOffset;
 
+  /// The (optional) index (starting from the top of the file) of the character
+  /// at which the cursor should be placed after implementing the completion.
+  ///
+  /// Quick fixes that alter multiple lines of editor content can't simply
+  /// provide an offset on the cursor's current line to represent the position
+  /// to which the cursor should move. This field is provided as an alternative
+  /// for use with those fixes.
+  final int absoluteCursorPosition;
+
   List<SourceEdit> quickFixes = [];
 
-  Completion(this.value,
-      {this.displayString, this.type, this.cursorOffset, this.quickFixes});
+  Completion(
+    this.value, {
+    this.displayString,
+    this.type,
+    this.cursorOffset,
+    this.absoluteCursorPosition,
+    this.quickFixes,
+  });
 
   bool isSetterAndMatchesGetter(Completion other) =>
       displayString == other.displayString &&
