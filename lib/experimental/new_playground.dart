72a99a38659a2df5b5bd2172d4de08a699a19db1
John Ryan <ryjohn@google.com>
2019-08-20 16:10:39 -0700

    add buttons

--- lib/experimental/new_playground.dart
+++ lib/experimental/new_playground.dart
@@ -4,6 +4,12 @@
 
 library new_playground;
 
+import 'dart:html';
+
+import 'package:mdc_web/mdc_web.dart';
+
+import '../elements/elements.dart';
+
 Playground _playground;
 
 void init() {
@@ -11,5 +17,42 @@ void init() {
 }
 
 class Playground {
+  DButton newButton;
+  DButton resetButton;
+  DButton formatButton;
+  DButton shareButton;
+  DButton samplesButton;
+  DButton runButton;
+
+  Playground() {
+    _initializeButtons();
+    _registerMDCButtons();
+  }
+
+  void _initializeButtons() {
+    newButton = DButton(querySelector('#new-button'));
+    resetButton = DButton(querySelector('#reset-button'));
+    formatButton = DButton(querySelector('#format-button'));
+    shareButton = DButton(querySelector('#share-button'));
+    samplesButton = DButton(querySelector('#samples-dropdown-button'));
+    runButton = DButton(querySelector('#run-button'))..onClick.listen((e) {
+      _handleRun();
+    });
+
+  }
+
+  /// Adds a material ripple effect
+  void _registerMDCButtons() {
+    MDCRipple(newButton.element);
+    MDCRipple(resetButton.element);
+    MDCRipple(formatButton.element);
+    MDCRipple(shareButton.element);
+    MDCRipple(samplesButton.element);
+    MDCRipple(runButton.element);
+  }
+
+  void _handleRun() async {
+    print('Run');
+  }
 
 }
