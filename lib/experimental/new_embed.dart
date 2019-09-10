5d6852b1b78932c6b567268f617ccf84720bf46e
John Ryan <ryjohn@google.com>
2019-09-10 16:33:16 -0700

    hide 'more' menu if there are no tests (#1256)
    * hide 'more' menu if there are no tests
    
    show tests is the only option, so hide the menu.
    
    closes #1173
    
    * avoid more menu flashing

--- lib/experimental/new_embed.dart
+++ lib/experimental/new_embed.dart
@@ -579,6 +579,7 @@ major browsers, such as Firefox, Edge (dev channel), or Chrome.
     context.hint = sources['hint.txt'] ?? '';
     tabController.setTabVisibility(
         'test', context.testMethod.isNotEmpty && _showTestCode);
+    menuButton.hidden = context.testMethod.isEmpty;
     showHintButton?.hidden = context.hint.isEmpty;
     solutionTab?.toggleAttr('hidden', context.solution.isEmpty);
     editorIsBusy = false;
