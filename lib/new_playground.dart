d83f34ff34a4314441a9d78b7dd8bdae5b5c851b
John Ryan <ryjohn@google.com>
2019-09-30 11:34:52 -0700

    UX updates (#1291)
    * lowercase tabs
    
    matches embeds
    
    * add close button to bottom panel
    
    * improve analyzer warning contrast
    
    * don't flex tabs in web layout
    
    * fix css selector
    
    * use dark color for header
    
    * avoid dark blue link color
    
    * use default text on MDC buttons
    
    for dialogs
    
    * add dropdown icon, remove reset-button usage
    
    * change keyboard styles
    
    * add icons to menu buttons
    
    * disable gist description widget
    
    * spacing
    
    * rename titleEditable to titleElement

--- lib/new_playground.dart
+++ lib/new_playground.dart
@@ -65,10 +65,11 @@ class Playground implements GistContainer, GistController {
   MDCButton runButton;
   MDCButton editorConsoleTab;
   MDCButton editorDocsTab;
+  MDCButton closePanelButton;
   DElement editorPanelFooter;
   MDCMenu samplesMenu;
   Dialog dialog;
-  DContentEditable titleEditable;
+  DElement titleElement;
   MaterialTabController webLayoutTabController;
   DElement webTabBar;
 
@@ -98,18 +99,18 @@ class Playground implements GistContainer, GistController {
   Counter unreadConsoleCounter;
 
   Playground() {
-    _initDialog();
-    _initBusyLights();
-    _initGistNameHeader();
-    _initGistStorage();
-    _initButtons();
-    _initSamplesMenu();
-    _initSplitters();
-    _initTabs();
-    _initLayout();
-    _initConsoles();
     _initModules().then((_) {
       _initPlayground();
+      _initDialog();
+      _initBusyLights();
+      _initGistNameHeader();
+      _initGistStorage();
+      _initButtons();
+      _initSamplesMenu();
+      _initSplitters();
+      _initTabs();
+      _initLayout();
+      _initConsoles();
     });
   }
 
@@ -142,12 +143,8 @@ class Playground implements GistContainer, GistController {
 
   void _initGistNameHeader() {
     // Update the title on changes.
-    titleEditable = DContentEditable(querySelector('header .header-gist-name'));
-    bind(titleEditable.onChanged, editableGist.property('description'));
-    bind(editableGist.property('description'), titleEditable.textProperty);
-    editableGist.onDirtyChanged.listen((val) {
-      titleEditable.element.classes.toggle('dirty', val);
-    });
+    titleElement = DElement(querySelector('header .header-gist-name'));
+    bind(editableGist.property('description'), titleElement.textProperty);
   }
 
   void _initGistStorage() {
@@ -180,6 +177,8 @@ class Playground implements GistContainer, GistController {
       });
     editorConsoleTab = MDCButton(querySelector('#editor-panel-console-tab'));
     editorDocsTab = MDCButton(querySelector('#editor-panel-docs-tab'));
+    closePanelButton =
+        MDCButton(querySelector('#editor-panel-close-button'), isIcon: true);
     querySelector('#keyboard-button')
         .onClick
         .listen((_) => _showKeyboardDialog());
@@ -285,6 +284,7 @@ class Playground implements GistContainer, GistController {
     tabExpandController = TabExpandController(
       consoleButton: editorConsoleTab,
       docsButton: editorDocsTab,
+      closeButton: closePanelButton,
       docsElement: _leftDocPanel,
       consoleElement: _leftConsoleElement,
       topSplit: _editorHost,
@@ -917,8 +917,8 @@ class Playground implements GistContainer, GistController {
 /// Adds a ripple effect to material design buttons
 class MDCButton extends DButton {
   final MDCRipple ripple;
-  MDCButton(ButtonElement element)
-      : ripple = MDCRipple(element),
+  MDCButton(ButtonElement element, {bool isIcon = false})
+      : ripple = MDCRipple(element)..unbounded = isIcon,
         super(element);
 }
 
@@ -961,6 +961,7 @@ enum TabState {
 class TabExpandController {
   final MDCButton consoleButton;
   final MDCButton docsButton;
+  final MDCButton closeButton;
   final DElement console;
   final DElement docs;
   final Counter unreadCounter;
@@ -983,6 +984,7 @@ class TabExpandController {
   TabExpandController({
     @required this.consoleButton,
     @required this.docsButton,
+    @required this.closeButton,
     @required Element consoleElement,
     @required Element docsElement,
     @required this.topSplit,
@@ -1000,6 +1002,10 @@ class TabExpandController {
 
     _subscriptions.add(docsButton.onClick.listen((_) {
       toggleDocs();
+    }));
+
+    _subscriptions.add(closeButton.onClick.listen((_) {
+      _hidePanel();
     }));
   }
 
@@ -1034,6 +1040,7 @@ class TabExpandController {
     bottomSplit.classes.remove('border-top');
     consoleButton.toggleClass('active', true);
     _initSplitter();
+    closeButton.toggleAttr('hidden', false);
   }
 
   void _hidePanel() {
@@ -1044,6 +1051,7 @@ class TabExpandController {
     bottomSplit.classes.add('border-top');
     consoleButton.toggleClass('active', false);
     docsButton.toggleClass('active', false);
+    closeButton.toggleAttr('hidden', true);
   }
 
   void _showDocs() {
@@ -1052,6 +1060,7 @@ class TabExpandController {
     bottomSplit.classes.remove('border-top');
     docsButton.toggleClass('active', true);
     _initSplitter();
+    closeButton.toggleAttr('hidden', false);
   }
 
   void _initSplitter() {
