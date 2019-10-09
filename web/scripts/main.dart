f5c35c34645f9981807733020804f6bf3f668ea9
John Ryan <ryjohn@google.com>
2019-10-09 14:35:31 -0700

    move experimental/new_playground/main to scripts/main

--- web/scripts/main.dart
+++ web/scripts/main.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:dart_pad/playground.dart' as playground;
+import 'package:dart_pad/new_playground.dart' as playground;
 import 'package:logging/logging.dart';
 
 void main() {
