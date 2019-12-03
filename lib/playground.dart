3c9c13588c5397f2b0ba35a9c207775c6d76f62f
John Ryan <ryjohn@google.com>
2019-12-03 07:48:16 -0800

    rename lib

--- lib/playground.dart
+++ lib/playground.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library new_playground;
+library playground;
 
 import 'dart:async';
 import 'dart:html' hide Console;
