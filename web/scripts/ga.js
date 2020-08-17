0e7d3bce98ead64c9b23da93b0a9676956df082d
Andrew Brogdon <redbrogdon@google.com>
2020-08-17 15:11:43 -0700

    Setting cookie flags

--- web/scripts/ga.js
+++ web/scripts/ga.js
@@ -3,6 +3,9 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-26406144-18', 'auto');
+ga('create', 'UA-26406144-18', {
+  cookieFlags: 'secure;samesite=none',
+  cookieDomain: 'auto'
+});
 ga('set', 'referrer', document.referrer.split('?')[0]);
 ga('send', 'pageview');
