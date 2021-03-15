b8b1b439d7edcf47267ac4422365a9b7449a347d
John Ryan <ryjohn@google.com>
2021-03-15 16:47:28 -0700

    move logic into CodelabUi class, start stubbing out code

---
+++ lib/codelab.dart
@@ -0,0 +1,104 @@
+import 'dart:html';
+
+import 'package:dart_pad/util/query_params.dart';
+import 'package:markdown/markdown.dart' as markdown;
+import 'package:split/split.dart';
+
+import 'codelabs/codelabs.dart';
+import 'core/modules.dart';
+import 'modules/codemirror_module.dart';
+import 'modules/dart_pad_module.dart';
+import 'modules/dartservices_module.dart';
+
+CodelabUi _codelabUi;
+
+CodelabUi get codelabUi => _codelabUi;
+
+void init() {
+  _codelabUi = CodelabUi();
+}
+
+class CodelabUi {
+  Splitter splitter;
+  Splitter rightSplitter;
+
+  CodelabUi() {
+    _init();
+  }
+
+  Future<void> _init() async {
+    _initSplitters();
+    var codelab = await _loadCodelab();
+    _initHeader(codelab.name);
+    _initStepsPanel(codelab);
+    await _initModules();
+
+  }
+
+  Future<void> _initModules() async {
+    var modules = ModuleManager();
+
+    modules.register(DartPadModule());
+    modules.register(DartServicesModule());
+    modules.register(CodeMirrorModule());
+
+    await modules.start();
+  }
+
+
+  Future<Codelab> _loadCodelab() async {
+    var fetcher = await _getFetcher();
+    return await fetcher.getCodelab();
+  }
+
+  void _initSplitters() {
+    var stepsPanel = querySelector('#steps-panel');
+    var editorPanel = querySelector('#editor-panel');
+    var outputPanel = querySelector('#output-panel');
+    splitter = flexSplit(
+      [stepsPanel, editorPanel],
+      horizontal: true,
+      gutterSize: 6,
+      sizes: const [50, 50],
+      minSize: [100, 100],
+    );
+  }
+
+  void _initHeader(String name) {
+    querySelector('#codelab-name').text = name;
+  }
+
+  void _initStepsPanel(Codelab codelab) {
+    var div = querySelector('#steps-panel');
+    div.innerHtml = markdown.markdownToHtml(codelab.steps.first.instructions);
+  }
+
+  Future<CodelabFetcher> _getFetcher() async {
+    var webServer = queryParams.webServer;
+    if (webServer != null && webServer.isNotEmpty) {
+      var uri = Uri.parse(webServer);
+      return WebServerCodelabFetcher(uri);
+    }
+    var ghOwner = queryParams.githubOwner;
+    var ghRepo = queryParams.githubRepo;
+    var ghRef = queryParams.githubRef;
+    var ghPath = queryParams.githubPath;
+    if (ghOwner != null &&
+        ghOwner.isNotEmpty &&
+        ghRepo != null &&
+        ghRepo.isNotEmpty &&
+        ghRef != null &&
+        ghRef.isNotEmpty &&
+        ghPath != null &&
+        ghPath.isNotEmpty) {
+      return GithubCodelabFetcher(
+        owner: ghOwner,
+        repo: ghRepo,
+        ref: ghRef,
+        path: ghPath,
+      );
+    }
+    throw ('Invalid parameters provided. Use either "webserver" or '
+        '"gh_owner", "gh_repo", "gh_ref", and "gh_path"');
+  }
+}
--- web/scripts/codelabs.dart
+++ web/scripts/codelabs.dart
@@ -1,36 +1,8 @@
-import 'package:dart_pad/codelabs/codelabs.dart';
-import 'package:dart_pad/util/query_params.dart';
+import 'package:dart_pad/codelab.dart';
+import 'package:logging/logging.dart';
 
-Future main() async {
-  var fetcher = await getFetcher();
-  var codelab = await fetcher.getCodelab();
-}
+void main() {
+  init();
 
-Future<CodelabFetcher> getFetcher() async {
-  var webServer = queryParams.webServer;
-  if (webServer != null && webServer.isNotEmpty) {
-    var uri = Uri.parse(webServer);
-    return WebServerCodelabFetcher(uri);
-  }
-  var ghOwner = queryParams.githubOwner;
-  var ghRepo = queryParams.githubRepo;
-  var ghRef = queryParams.githubRef;
-  var ghPath = queryParams.githubPath;
-  if (ghOwner != null &&
-      ghOwner.isNotEmpty &&
-      ghRepo != null &&
-      ghRepo.isNotEmpty &&
-      ghRef != null &&
-      ghRef.isNotEmpty &&
-      ghPath != null &&
-      ghPath.isNotEmpty) {
-    return GithubCodelabFetcher(
-      owner: ghOwner,
-      repo: ghRepo,
-      ref: ghRef,
-      path: ghPath,
-    );
-  }
-  throw ('Invalid parameters provided. Use either "webserver" or '
-      '"gh_owner", "gh_repo", "gh_ref", and "gh_path"');
+  Logger.root.onRecord.listen(print);
 }
