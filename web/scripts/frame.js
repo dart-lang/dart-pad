f38709feea537c9bff77690bfe515bb23cb8023b
Sam Rawlins <srawlins@google.com>
2021-04-21 10:55:55 -0700

    Deduplicate JavaScript in frames

---
+++ web/scripts/frame.js
@@ -0,0 +1,36 @@
+
+replaceJavaScript = function (value) {
+    // Remove the old node.
+    var oldNode = document.getElementById('compiledJsScript');
+    if (oldNode && oldNode.parentNode) {
+        oldNode.parentNode.removeChild(oldNode);
+    }
+
+    // Create a new node.
+    var scriptNode = document.createElement('script');
+    scriptNode.setAttribute('id', 'compiledJsScript');
+    scriptNode.text = value;
+    document.head.appendChild(scriptNode);
+};
+
+messageHandler = function (e) {
+    var obj = e.data;
+    var command = obj.command;
+    var body = document.body;
+
+    if (command === 'setCss') {
+        document.getElementById('styleId').innerHTML = obj.css;
+    } else if (command === 'setHtml') {
+        body.innerHTML = obj.html;
+    } else if (command === 'execute') {
+        // Replace all three.
+        body.innerHTML = obj.html;
+        document.getElementById('styleId').innerHTML = obj.css;
+        replaceJavaScript(obj.js);
+    }
+};
+
+window.addEventListener('load', function () {
+    window.addEventListener('message', messageHandler, false);
+    parent.postMessage({ 'sender': 'frame', 'type': 'ready' }, '*');
+});
