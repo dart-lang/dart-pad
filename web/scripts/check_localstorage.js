344417ae3b1b063006a4635adafd00aa5a592d64
Sam Rawlins <srawlins@google.com>
2021-04-23 13:09:59 -0700

    Add copyrights

--- web/scripts/check_localstorage.js
+++ web/scripts/check_localstorage.js
@@ -1,5 +1,10 @@
-// This function is used by lib/check_localstorage.dart
+/**
+ * Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
 
+// This function is used by lib/check_localstorage.dart
 function checkLocalStorage() {
     try {
         let key = 'O09DGzSpoH';
