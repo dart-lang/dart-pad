516fe43b1c564a09956cea67ac2f90403c622434
Brett Morgan <brett.morgan@gmail.com>
2021-06-27 14:50:43 +1000

    Drop unused complexity (#736)

--- example/web/index.dart
+++ example/web/index.dart
@@ -11,8 +11,6 @@ import 'package:http/browser_client.dart';
 import 'package:http/http.dart';
 import 'services_utils.dart' as utils;
 
-const versions = ['v2'];
-
 BrowserClient _client;
 
 void main() {
@@ -45,171 +43,133 @@ Future<Response> get(String url) async {
 }
 
 void setupAnalyze() {
-  versions.forEach((version) {
-    final editor =
-        createEditor(querySelector('#analyzeSection-$version .editor'));
-    final output = querySelector('#analyzeSection-$version .output');
-    final button =
-        querySelector('#analyzeSection-$version button') as ButtonElement;
-    button.onClick.listen((e) {
-      final source = {'source': editor.getDoc().getValue()};
-      final sw = Stopwatch()..start();
-      post(
-        '$_uriBase/dartservices/$version/analyze',
-        body: json.encode(source),
-      ).then(
-          (response) => output.text = '${_formatTiming(sw)}${response.body}');
-    });
+  final editor = createEditor(querySelector('#analyzeSection-v2 .editor'));
+  final output = querySelector('#analyzeSection-v2 .output');
+  final button = querySelector('#analyzeSection-v2 button') as ButtonElement;
+  button.onClick.listen((e) {
+    final source = {'source': editor.getDoc().getValue()};
+    final sw = Stopwatch()..start();
+    post(
+      '$_uriBase/dartservices/v2/analyze',
+      body: json.encode(source),
+    ).then((response) => output.text = '${_formatTiming(sw)}${response.body}');
   });
 }
 
 void setupAssists() {
-  versions.forEach((version) {
-    final editor =
-        createEditor(querySelector('#assistsSection-$version .editor'));
-    final output = querySelector('#assistsSection-$version .output');
-    final offsetElement = querySelector('#assistsSection-$version .offset');
-    final button =
-        querySelector('#assistsSection-$version button') as ButtonElement;
-    button.onClick.listen((e) {
-      final source = _getSourceRequest(editor);
-      final sw = Stopwatch()..start();
-      post(
-        '$_uriBase/dartservices/$version/assists',
-        body: json.encode(source),
-      ).then(
-          (response) => output.text = '${_formatTiming(sw)}${response.body}');
-    });
+  final editor = createEditor(querySelector('#assistsSection-v2 .editor'));
+  final output = querySelector('#assistsSection-v2 .output');
+  final offsetElement = querySelector('#assistsSection-v2 .offset');
+  final button = querySelector('#assistsSection-v2 button') as ButtonElement;
+  button.onClick.listen((e) {
+    final source = _getSourceRequest(editor);
+    final sw = Stopwatch()..start();
+    post(
+      '$_uriBase/dartservices/v2/assists',
+      body: json.encode(source),
+    ).then((response) => output.text = '${_formatTiming(sw)}${response.body}');
+  });
+  offsetElement.text = 'offset ${_getOffset(editor)}';
+  editor.onCursorActivity.listen((_) {
     offsetElement.text = 'offset ${_getOffset(editor)}';
-    editor.onCursorActivity.listen((_) {
-      offsetElement.text = 'offset ${_getOffset(editor)}';
-    });
   });
 }
 
 void setupCompile() {
-  versions.forEach((version) {
-    final editor =
-        createEditor(querySelector('#compileSection-$version .editor'));
-    final output = querySelector('#compileSection-$version .output');
-    final button =
-        querySelector('#compileSection-$version button') as ButtonElement;
-    button.onClick.listen((e) {
-      final source = editor.getDoc().getValue();
-      final compile = {'source': source};
-      final sw = Stopwatch()..start();
-      post(
-        '$_uriBase/dartservices/$version/compile',
-        body: json.encode(compile),
-      ).then(
-          (response) => output.text = '${_formatTiming(sw)}${response.body}');
-    });
+  final editor = createEditor(querySelector('#compileSection-v2 .editor'));
+  final output = querySelector('#compileSection-v2 .output');
+  final button = querySelector('#compileSection-v2 button') as ButtonElement;
+  button.onClick.listen((e) {
+    final source = editor.getDoc().getValue();
+    final compile = {'source': source};
+    final sw = Stopwatch()..start();
+    post(
+      '$_uriBase/dartservices/v2/compile',
+      body: json.encode(compile),
+    ).then((response) => output.text = '${_formatTiming(sw)}${response.body}');
   });
 }
 
 void setupCompileDDC() {
-  versions.forEach((version) {
-    final editor =
-        createEditor(querySelector('#compileDDCSection-$version .editor'));
-    final output = querySelector('#compileDDCSection-$version .output');
-    final button =
-        querySelector('#compileDDCSection-$version button') as ButtonElement;
-    button.onClick.listen((e) {
-      final source = editor.getDoc().getValue();
-      final compile = {'source': source};
-      final sw = Stopwatch()..start();
-      post(
-        '$_uriBase/dartservices/$version/compileDDC',
-        body: json.encode(compile),
-      ).then(
-          (response) => output.text = '${_formatTiming(sw)}${response.body}');
-    });
+  final editor = createEditor(querySelector('#compileDDCSection-v2 .editor'));
+  final output = querySelector('#compileDDCSection-v2 .output');
+  final button = querySelector('#compileDDCSection-v2 button') as ButtonElement;
+  button.onClick.listen((e) {
+    final source = editor.getDoc().getValue();
+    final compile = {'source': source};
+    final sw = Stopwatch()..start();
+    post(
+      '$_uriBase/dartservices/v2/compileDDC',
+      body: json.encode(compile),
+    ).then((response) => output.text = '${_formatTiming(sw)}${response.body}');
   });
 }
 
 void setupComplete() {
-  versions.forEach((version) {
-    final editor =
-        createEditor(querySelector('#completeSection-$version .editor'));
-    final output = querySelector('#completeSection-$version .output');
-    final offsetElement = querySelector('#completeSection-$version .offset');
-    final button =
-        querySelector('#completeSection-$version button') as ButtonElement;
-    button.onClick.listen((e) {
-      final source = _getSourceRequest(editor);
-      final sw = Stopwatch()..start();
-      post(
-        '$_uriBase/dartservices/$version/complete',
-        body: json.encode(source),
-      ).then(
-          (response) => output.text = '${_formatTiming(sw)}${response.body}');
-    });
+  final editor = createEditor(querySelector('#completeSection-v2 .editor'));
+  final output = querySelector('#completeSection-v2 .output');
+  final offsetElement = querySelector('#completeSection-v2 .offset');
+  final button = querySelector('#completeSection-v2 button') as ButtonElement;
+  button.onClick.listen((e) {
+    final source = _getSourceRequest(editor);
+    final sw = Stopwatch()..start();
+    post(
+      '$_uriBase/dartservices/v2/complete',
+      body: json.encode(source),
+    ).then((response) => output.text = '${_formatTiming(sw)}${response.body}');
+  });
+  offsetElement.text = 'offset ${_getOffset(editor)}';
+  editor.onCursorActivity.listen((_) {
     offsetElement.text = 'offset ${_getOffset(editor)}';
-    editor.onCursorActivity.listen((_) {
-      offsetElement.text = 'offset ${_getOffset(editor)}';
-    });
   });
 }
 
 void setupDocument() {
-  versions.forEach((version) {
-    final editor =
-        createEditor(querySelector('#documentSection-$version .editor'));
-    final output = querySelector('#documentSection-$version .output');
-    final offsetElement = querySelector('#documentSection-$version .offset');
-    final button =
-        querySelector('#documentSection-$version button') as ButtonElement;
-    button.onClick.listen((e) {
-      final source = _getSourceRequest(editor);
-      final sw = Stopwatch()..start();
-      post(
-        '$_uriBase/dartservices/$version/document',
-        body: json.encode(source),
-      ).then(
-          (response) => output.text = '${_formatTiming(sw)}${response.body}');
-    });
+  final editor = createEditor(querySelector('#documentSection-v2 .editor'));
+  final output = querySelector('#documentSection-v2 .output');
+  final offsetElement = querySelector('#documentSection-v2 .offset');
+  final button = querySelector('#documentSection-v2 button') as ButtonElement;
+  button.onClick.listen((e) {
+    final source = _getSourceRequest(editor);
+    final sw = Stopwatch()..start();
+    post(
+      '$_uriBase/dartservices/v2/document',
+      body: json.encode(source),
+    ).then((response) => output.text = '${_formatTiming(sw)}${response.body}');
+  });
+  offsetElement.text = 'offset ${_getOffset(editor)}';
+  editor.onCursorActivity.listen((_) {
     offsetElement.text = 'offset ${_getOffset(editor)}';
-    editor.onCursorActivity.listen((_) {
-      offsetElement.text = 'offset ${_getOffset(editor)}';
-    });
   });
 }
 
 void setupFixes() {
-  versions.forEach((version) {
-    final editor =
-        createEditor(querySelector('#fixesSection-$version .editor'));
-    final output = querySelector('#fixesSection-$version .output');
-    final offsetElement = querySelector('#fixesSection-$version .offset');
-    final button =
-        querySelector('#fixesSection-$version button') as ButtonElement;
-    button.onClick.listen((e) {
-      final source = _getSourceRequest(editor);
-      final sw = Stopwatch()..start();
-      post(
-        '$_uriBase/dartservices/$version/fixes',
-        body: json.encode(source),
-      ).then(
-          (response) => output.text = '${_formatTiming(sw)}${response.body}');
-    });
-    offsetElement.text = 'offset ${_getOffset(editor)}';
+  final editor = createEditor(querySelector('#fixesSection-v2 .editor'));
+  final output = querySelector('#fixesSection-v2 .output');
+  final offsetElement = querySelector('#fixesSection-v2 .offset');
+  final button = querySelector('#fixesSection-v2 button') as ButtonElement;
+  button.onClick.listen((e) {
+    final source = _getSourceRequest(editor);
+    final sw = Stopwatch()..start();
+    post(
+      '$_uriBase/dartservices/v2/fixes',
+      body: json.encode(source),
+    ).then((response) => output.text = '${_formatTiming(sw)}${response.body}');
+  });
+  offsetElement.text = 'offset ${_getOffset(editor)}';
 
-    editor.onCursorActivity.listen((_) {
-      offsetElement.text = 'offset ${_getOffset(editor)}';
-    });
+  editor.onCursorActivity.listen((_) {
+    offsetElement.text = 'offset ${_getOffset(editor)}';
   });
 }
 
 void setupVersion() {
-  versions.forEach((version) {
-    final output = querySelector('#versionSection-$version .output');
-    final button =
-        querySelector('#versionSection-$version button') as ButtonElement;
-    button.onClick.listen((e) {
-      final sw = Stopwatch()..start();
-      get('$_uriBase/dartservices/$version/version').then(
-          (response) => output.text = '${_formatTiming(sw)}${response.body}');
-    });
+  final output = querySelector('#versionSection-v2 .output');
+  final button = querySelector('#versionSection-v2 button') as ButtonElement;
+  button.onClick.listen((e) {
+    final sw = Stopwatch()..start();
+    get('$_uriBase/dartservices/v2/version').then(
+        (response) => output.text = '${_formatTiming(sw)}${response.body}');
   });
 }
 
