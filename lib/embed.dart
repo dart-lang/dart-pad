476c959d377c940d906bbae3378c6f431789078c
Matej Zidek <zidek.matt@gmail.com>
2021-06-30 22:04:18 +0200

    fix null safety errors when console view in embeds was displayed
    embed_flutter.dart.js:666 Uncaught TypeError: Instance of 'minified:F<minified:B?>': type 'minified:F<minified:B?>' is not a subtype of type 'List<minified:B>'
    
    late initialization error _splitter

--- lib/embed.dart
+++ lib/embed.dart
@@ -1160,7 +1160,7 @@ class ConsoleExpandController extends Console {
   final Counter? unreadCounter;
   final Function? onSizeChanged;
   final EditorUi? editorUi;
-  late Splitter _splitter;
+  Splitter? _splitter;
   bool _expanded;
 
   ConsoleExpandController({
@@ -1212,18 +1212,18 @@ class ConsoleExpandController extends Console {
     _expanded = !_expanded;
     if (_expanded) {
       _initSplitter();
-      _splitter.setSizes([60, 40]);
+      _splitter?.setSizes([60, 40]);
       element.toggleAttr('hidden', false);
       expandIcon.element.innerText = 'expand_more';
       footer.toggleClass('footer-top-border', false);
       unreadCounter!.clear();
     } else {
-      _splitter.setSizes([100, 0]);
+      _splitter?.setSizes([100, 0]);
       element.toggleAttr('hidden', true);
       expandIcon.element.innerText = 'expand_less';
       footer.toggleClass('footer-top-border', true);
       try {
-        _splitter.destroy();
+        _splitter?.destroy();
       } on NoSuchMethodError {
         // dart2js throws NoSuchMethodError (dartdevc is ok)
         // TODO(ryjohn): why does this happen?
@@ -1234,11 +1234,11 @@ class ConsoleExpandController extends Console {
 
   void _initSplitter() {
     var splitterElements = [
-      querySelector('#editor-container'),
-      querySelector('#console-output-footer'),
+      querySelector('#editor-container')!,
+      querySelector('#console-output-footer')!,
     ];
     _splitter = flexSplit(
-      splitterElements as List<Element>,
+      splitterElements,
       horizontal: false,
       gutterSize: defaultSplitterWidth,
       sizes: [60, 40],
