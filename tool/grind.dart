eafc655950d84f10270b1fb45bfa42907f5f9dac
Sam Rawlins <srawlins@google.com>
2021-09-16 13:27:40 -0700

    Bump 'null safety'-enabled language version to 2.14 (#750)

--- tool/grind.dart
+++ tool/grind.dart
@@ -428,7 +428,7 @@ String createPubspec({
   var content = '''
 name: $_samplePackageName
 environment:
-  sdk: '>=${nullSafety ? '2.13.0' : '2.10.0'} <3.0.0'
+  sdk: '>=${nullSafety ? '2.14.0' : '2.10.0'} <3.0.0'
 dependencies:
 ''';
 
