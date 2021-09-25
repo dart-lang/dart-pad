078afe7ca232e683e982fabcd6c4b06599bd38b6
Brett Morgan <brett.morgan@gmail.com>
2021-09-25 23:08:49 +1000

    Revert "Use the dart docker image (#753)" (#754)
    This reverts commit 1d71245e23376d7450d1eff2155b65abe472f27b.

--- tool/grind.dart
+++ tool/grind.dart
@@ -53,7 +53,7 @@ Future<void> serveNullSafety() async {
       arguments: ['bin/server_dev.dart', '--port', '8084', '--null-safety']);
 }
 
-const _dartImageName = 'dart';
+const _dartImageName = 'google/dart';
 final _dockerVersionMatcher = RegExp('^FROM $_dartImageName:(.*)\$');
 const _dockerFileNames = [
   'cloud_run.Dockerfile',
