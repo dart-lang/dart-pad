642915cec3b056604b4628c58e3d48601567d0d3
John Ryan <ryjohn@google.com>
2022-11-02 14:49:51 -0700

    Add UTM

--- lib/playground.dart
+++ lib/playground.dart
@@ -809,7 +809,7 @@ class Playground extends EditorUi implements GistContainer, GistController {
       dialog.showOk(
         'Install Flutter',
         r'Success! '
-            r'<a class="link-bright" href="https://docs.flutter.dev/get-started/install">'
+            r'<a class="link-bright" href="https://docs.flutter.dev/get-started/install?utm_campaign=dartpad_install_popup">'
             r'click here</a>'
             r' to install the Flutter SDK and unlock more features like Hot Reload.',
       );
