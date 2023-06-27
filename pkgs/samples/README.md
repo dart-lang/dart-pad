d0ba0b9c292025f5b7ca038b7a9e5f0a2b5badfd
Devon Carew <devoncarew@google.com>
2023-06-27 14:49:49 -0700

    contribute a samples project for dartpad

---
+++ pkgs/samples/README.md
@@ -0,0 +1,24 @@
+[![samples](https://github.com/dart-lang/dart-pad/actions/workflows/samples.yml/badge.svg)](https://github.com/dart-lang/dart-pad/actions/workflows/samples.yml)
+
+Sample code snippets for DartPad.
+
+## Samples
+
+<!-- samples -->
+| Category | Name | | ID | Source |
+| --- | --- | --- | --- |
+| Dart | Hello world | `hello-world` | [lib/hello_world.dart](lib/hello_world.dart) |
+| Dart | Fibonacci | `fibonacci` | [lib/fibonacci.dart](lib/fibonacci.dart) |
+| Flutter | Counter example | `counter-example` | [lib/main.dart](lib/main.dart) |
+<!-- samples -->
+
+## Contributing
+
+When considering contributing a sample to DartPad, please open an issue first
+for discussion.
+
+After the sample is discussed and approved:
+
+- add the code for the sample to a new Dart file in `lib/`
+- add an entry for the sample to the [lib/samples.json](lib/samples.json) file
+- run `dart tool/samples.dart` to re-generate related files
---
+++ pkgs/samples/lib/main.dart
@@ -0,0 +1,73 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
