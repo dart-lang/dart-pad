a3440009166576f482de4b5b5198646245ee5718
Devon Carew <devoncarew@gmail.com>
2023-08-15 12:54:23 -0700

    update the protos to use ctor named params (#2619)

--- pkgs/dart_services/lib/src/protos/dart_services.pbjson.dart
+++ pkgs/dart_services/lib/src/protos/dart_services.pbjson.dart
@@ -4,7 +4,7 @@
 //
 // @dart = 2.12
 
-// ignore_for_file: annotate_overrides, camel_case_types
+// ignore_for_file: annotate_overrides, camel_case_types, comment_references
 // ignore_for_file: constant_identifier_names, library_prefixes
 // ignore_for_file: non_constant_identifier_names, prefer_final_fields
 // ignore_for_file: unnecessary_import, unnecessary_this, unused_import
