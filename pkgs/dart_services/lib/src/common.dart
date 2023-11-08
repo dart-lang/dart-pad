3b3b3c073de31146bb94063ecedcdea231559f67
John Ryan <ryjohn@google.com>
2023-11-08 09:11:51 -0800

    Fix unit tests

--- pkgs/dart_services/lib/src/common.dart
+++ pkgs/dart_services/lib/src/common.dart
@@ -69,9 +69,11 @@ void main() async {
 const sampleCodeFlutterCounter = r'''
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -87,7 +89,10 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
