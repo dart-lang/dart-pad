e962e3697a72b377db2c1a6a4fb3ea1e418489f0
John Ryan <ryjohn@google.com>
2023-11-21 13:25:07 -0800

    unused import

--- pkgs/sketch_pad/lib/widgets.dart
+++ pkgs/sketch_pad/lib/widgets.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
