c613315f593b8d2dafcfa6f29879dc36307bf16a
Parker Lougheed <parlough@gmail.com>
2023-12-26 14:46:19 -0600

    Add documentation to frame_utils extension

--- pkgs/sketch_pad/lib/execution/frame_utils.dart
+++ pkgs/sketch_pad/lib/execution/frame_utils.dart
@@ -1,13 +1,23 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:js_interop';
 import 'dart:js_interop_unsafe';
 
 import 'package:web/web.dart';
 
+/// Extensions to work around Dart compiler issues that result
+/// in calls that sandboxed iframes error on.
 extension HTMLIFrameElementExtension on HTMLIFrameElement {
+  /// Send the specified [message] to this iframe,
+  /// configured with the specified [optionsOrTargetOrigin].
   void safelyPostMessage(
     JSAny? message,
     String optionsOrTargetOrigin,
   ) {
+    // Uses unsafe calls to prevent the Dart web compilers from
+    // inserting type or null checks that access restricted properties.
     (this as JSObject)
         .getProperty<JSObject>('contentWindow'.toJS)
         .callMethod('postMessage'.toJS, message, optionsOrTargetOrigin.toJS);
