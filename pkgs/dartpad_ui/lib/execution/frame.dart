790eb088efa9dfb866a30b6755d3a54ebbfa8733
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-06-12 01:19:59 +0100

    Update frame.dart

--- pkgs/dartpad_ui/lib/execution/frame.dart
+++ pkgs/dartpad_ui/lib/execution/frame.dart
@@ -165,7 +165,7 @@ require(["dartpad_main", "dart_sdk"], function(dartpad_main, dart_sdk) {
         (web.Event event) {
           if (event is web.MessageEvent) {
             final data = event.data.dartify() as Map<Object?, Object?>;
-            if (data['sender'] != 'frame' || event.source == null || event.source != _frame.contentWindow) {
+            if (data['sender'] != 'frame' || _frame.contentWindow == null || event.source != _frame.contentWindow) {
               return;
             }
             final type = data['type'] as String?;
