2e6bcfd9a6aa1956f1e071fd2b7eeb955f0b6869
Chris Sells <csells@sellsbrothers.com>
2025-02-02 19:21:42 -0800

    license header fixes

--- pkgs/dartpad_ui/lib/suggest_fix.dart
+++ pkgs/dartpad_ui/lib/suggest_fix.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:dartpad_shared/model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
