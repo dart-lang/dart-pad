71ca57c53d40e2dd33b05e7b8e73fce1fbb6cb32
Devon Carew <devoncarew@google.com>
2025-03-03 09:00:45 -0800

    add file copyright

--- pkgs/dartpad_ui/lib/enable_gen_ai.dart
+++ pkgs/dartpad_ui/lib/enable_gen_ai.dart
@@ -1,2 +1,6 @@
-// turn on or off gen-ai features in the client
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Turn on or off gen-ai features in the client.
 const bool genAiEnabled = false;
