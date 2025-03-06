00dc3cd4d62294c1f74f8a7802221f9bfaba3f6f
Polina Cherkasova <polina.c@live.com>
2025-03-06 09:35:38 -0800

    Update flutter_genui.dart

--- pkgs/dart_services/lib/src/flutter_genui.dart
+++ pkgs/dart_services/lib/src/flutter_genui.dart
@@ -6,30 +6,34 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-Future<void> invokeFlutterGenUi({
+Future<String> invokeFlutterGenUi({
   required String prompt,
   String? apiKey,
 }) async {
   final response = await _requestGenui(prompt: prompt, apiKey: apiKey);
-  if (response.statusCode == 200) {
-    print('Success: ${response.body}');
-  } else {
-    print('Failed: ${response.statusCode}');
+  if (response.statusCode != 200) {
+    throw Exception('Failed to generate ui: ${response.body}');
   }
+
+  final decoded = jsonDecode(response.body) as Map<String, dynamic>;
+
+  print(decoded);
+
+  return decoded.toString();
 }
 
 Future<http.Response> _requestGenui({required String prompt, String? apiKey}) {
-  final keyParem = apiKey == null ? '' : '?key=$apiKey';
+  final keyParam = apiKey == null ? '' : '?key=$apiKey';
 
   return http.post(
     Uri.parse(
-      'https://autopush-devgenui.sandbox.googleapis.com/v1beta1/firstparty/generateidecode$keyParem',
+      'https://autopush-devgenui.sandbox.googleapis.com/v1beta1/firstparty/generateidecode$keyParam',
     ),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
     body: jsonEncode(<String, String>{
-      'userPrompt': 'hello, genui',
+      'userPrompt': prompt,
       'modelUrl': 'genuigemini://models/gemini-2.0-flash',
     }),
   );
