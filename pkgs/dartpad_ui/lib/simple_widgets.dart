1064e8b6117e5d08ef8bc2fe0fb2da25d05043ca
Polina Cherkasova <polina.c@live.com>
2025-04-11 14:54:11 -0700

    -

--- pkgs/dartpad_ui/lib/simple_widgets.dart
+++ pkgs/dartpad_ui/lib/simple_widgets.dart
@@ -5,7 +5,6 @@
 import 'dart:async';
 
 import 'package:dartpad_shared/model.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
