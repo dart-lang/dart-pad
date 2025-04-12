eaeca4680c270f2e194d04f3f657f43777937843
Polina Cherkasova <polina.c@live.com>
2025-04-11 19:05:41 -0700

    Update edit_with_ai.dart

--- pkgs/dartpad_ui/lib/edit_with_ai.dart
+++ pkgs/dartpad_ui/lib/edit_with_ai.dart
@@ -7,11 +7,11 @@ import 'package:flutter/material.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
 
 import 'docs.dart';
+import 'editor/editor.dart';
 import 'model.dart';
 import 'problems.dart';
 import 'simple_widgets.dart';
 import 'theme.dart';
-import 'editor/editor.dart';
 
 class EditorWithButtons extends StatelessWidget {
   const EditorWithButtons({
