3ad2ebd956b46927472a513ce075c312e1ce1efc
Polina Cherkasova <polina.c@live.com>
2025-04-16 09:02:28 -0700

    -

--- pkgs/dartpad_ui/lib/embed.dart
+++ pkgs/dartpad_ui/lib/embed.dart
@@ -2,42 +2,42 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:js_interop';
+// import 'dart:js_interop';
 
-import 'package:web/web.dart' as web;
+// import 'package:web/web.dart' as web;
 
 import 'model.dart';
 
 /// Listen to frame messages if embedded as an iFrame
 /// to accept injected snippets.
 void handleEmbedMessage(AppServices services, {bool runOnInject = false}) {
-  final parent = web.window.parentCrossOrigin;
-  if (parent == null) return;
+  // final parent = web.window.parentCrossOrigin;
+  // if (parent == null) return;
 
-  web.window.addEventListener(
-    'message',
-    (web.MessageEvent event) {
-      if (event.data case _SourceCodeMessage(
-        :final type?,
-        :final sourceCode?,
-      ) when type == 'sourceCode') {
-        if (sourceCode.isNotEmpty) {
-          services.appModel.sourceCodeController.text = sourceCode;
-          if (runOnInject) {
-            services.performCompileAndRun();
-          }
-        }
-      }
-    }.toJS,
-  );
+  // web.window.addEventListener(
+  //   'message',
+  //   (web.MessageEvent event) {
+  //     if (event.data case _SourceCodeMessage(
+  //       :final type?,
+  //       :final sourceCode?,
+  //     ) when type == 'sourceCode') {
+  //       if (sourceCode.isNotEmpty) {
+  //         services.appModel.sourceCodeController.text = sourceCode;
+  //         if (runOnInject) {
+  //           services.performCompileAndRun();
+  //         }
+  //       }
+  //     }
+  //   }.toJS,
+  // );
 
-  parent.postMessage(
-    {'sender': web.window.name, 'type': 'ready'}.jsify(),
-    '*'.toJS,
-  );
+  // parent.postMessage(
+  //   {'sender': web.window.name, 'type': 'ready'}.jsify(),
+  //   '*'.toJS,
+  // );
 }
 
-extension type _SourceCodeMessage._(JSObject _) {
-  external String? get sourceCode;
-  external String? get type;
-}
+// extension type _SourceCodeMessage._(JSObject _) {
+//   external String? get sourceCode;
+//   external String? get type;
+// }
