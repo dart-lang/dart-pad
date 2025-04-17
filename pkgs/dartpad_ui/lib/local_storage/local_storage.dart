096d7063a0fff12c978a62a03f23ad02dd7fbf7f
Polina Cherkasova <polina.c@live.com>
2025-04-16 20:46:38 -0700

    -

--- pkgs/dartpad_ui/lib/local_storage/local_storage.dart
+++ pkgs/dartpad_ui/lib/local_storage/local_storage.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'stub.dart' if (dart.library.js_util) 'web.dart';
+import '_stub.dart' if (dart.library.js_util) '_web.dart';
 
 abstract class LocalStorage {
   static LocalStorageImpl instance = LocalStorageImpl();
