4824fe38b92c6e4b03ec1435349f494df1f6f5f7
Polina Cherkasova <polina.c@live.com>
2025-05-01 10:53:09 -0700

    Update main_test.dart

--- pkgs/dartpad_ui/test/presubmit/main_test.dart
+++ pkgs/dartpad_ui/test/presubmit/main_test.dart
@@ -3,10 +3,8 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:dart_services/server.dart';
-import 'package:dartpad_shared/services.dart';
 import 'package:dartpad_ui/genai_editing.dart';
 import 'package:dartpad_ui/main.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
