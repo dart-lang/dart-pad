188bd66726be15fdcfb51e64bdb63773c0a856d9
Polina Cherkasova <polina.c@live.com>
2025-05-08 13:41:29 -0700

    Replace `localhost` with `127.0.0.1`.  (#3286)

---
+++ pkgs/dartpad_shared/lib/constants.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// The IP address of the localhost.
+/// For some reasons just `localhost` does not work for
+/// unit testing of ui against local backend.
+const localhostIp = '127.0.0.1';
