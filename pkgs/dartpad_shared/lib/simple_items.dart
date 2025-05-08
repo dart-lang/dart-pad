325a0f55e90ae6823d0aba9d6f7deda075345896
Polina Cherkasova <polina.c@live.com>
2025-05-08 08:53:22 -0700

    Update simple_items.dart

--- pkgs/dartpad_shared/lib/simple_items.dart
+++ pkgs/dartpad_shared/lib/simple_items.dart
@@ -2,4 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// The IP address of the localhost.
+/// For some reasons just `localhost` does not work for
+/// unit testing of ui against local backend.
 const localhostIp = '127.0.0.1';
