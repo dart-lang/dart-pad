d47956f2c63c36b3297b131fa449e70dfdd39bce
Polina Cherkasova <polina.c@live.com>
2025-05-09 10:23:46 -0700

    -

--- pkgs/dartpad_ui/test/main_test.dart
+++ pkgs/dartpad_ui/test/main_test.dart
@@ -2,20 +2,31 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:dart_services/server.dart';
 import 'package:dartpad_ui/genai_editing.dart';
 import 'package:dartpad_ui/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import '../test_infra/test_utils.dart';
+import 'test_infra/test_utils.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  final runner = TestServerRunner();
+
+  setUpAll(() async {
+    await runner.start();
+  });
+
+  tearDownAll(() async {
+    await runner.stop();
+  });
+
   testWidgets('Initial screen.', (WidgetTester tester) async {
     await setMinLargeScreenWidth(tester);
 
-    await tester.pumpWidget(const DartPadApp());
+    await tester.pumpWidget(const DartPadApp(channel: 'localhost'));
 
     final DartPadMainPageState state = tester.state(
       find.byType(DartPadMainPage),
@@ -24,5 +35,10 @@ void main() {
     await state.initialized.future;
     await tester.pumpAndSettle();
     await expectLater(find.byType(EditorWithButtons), findsOneWidget);
+
+    await expectLater(
+      find.byType(DartPadApp),
+      matchesGoldenFile('$goldenPath/main_screen_initial.png'),
+    );
   });
 }
