d8c6daa1683de999433d6830e3b630523d78f108
Polina Cherkasova <polina.c@live.com>
2025-05-12 11:17:18 -0700

    Update ai_test.dart

--- pkgs/dartpad_ui/test/ai_test.dart
+++ pkgs/dartpad_ui/test/ai_test.dart
@@ -2,13 +2,15 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:dart_services/server.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 
-import '../test_infra/test_utils.dart';
+import 'test_infra/test_utils.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  setUpAll(() async {
+    await TestServerRunner().maybeStart();
+  });
 
   testWidgets('AI CUJ', (WidgetTester tester) async {
     await initializeMainPage(tester);
