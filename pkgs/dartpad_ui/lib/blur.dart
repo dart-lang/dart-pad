8faee82650647cfe2e3c6cb038565361707a4644
John Ryan <ryjohn@google.com>
2025-05-29 11:24:08 -0700

    Fix copyright header

--- pkgs/dartpad_ui/lib/blur.dart
+++ pkgs/dartpad_ui/lib/blur.dart
@@ -1,6 +1,6 @@
-// Copyright 2025 The Flutter Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
+// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 import 'dart:js_interop';
 
