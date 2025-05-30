be36d5cab05ac32e1c1879d180b066944c6c5f8b
Polina Cherkasova <polina.c@live.com>
2025-05-29 19:28:13 -0700

    Update server_test.dart

--- pkgs/dart_services/test/probes/server_test.dart
+++ pkgs/dart_services/test/probes/server_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@Timeout(Duration(seconds: 60))
+library;
+
 import 'package:test/test.dart';
 
 import '../probes_and_presubmit/server_testing.dart';
