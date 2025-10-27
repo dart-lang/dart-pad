420c6d2502f0acc86014abdb169eada7257159ef
Devon Carew <devoncarew@google.com>
2025-10-27 13:34:20 -0700

    ignore deprecated services in tests

--- pkgs/dart_services/test/probes_and_presubmit/reload_testing.dart
+++ pkgs/dart_services/test/probes_and_presubmit/reload_testing.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// ignore_for_file: deprecated_member_use
+
 import 'package:dartpad_shared/services.dart';
 
 import 'ddc_testing.dart';
