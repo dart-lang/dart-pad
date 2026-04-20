0865358f5204868d9c7e0dd7facca244b7d279b5
Parker Lougheed <parlough@gmail.com>
2026-04-20 18:23:59 +0200

    Update pub deps across all channels (#3601)

--- pkgs/dartpad_ui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pkgs/dartpad_ui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
