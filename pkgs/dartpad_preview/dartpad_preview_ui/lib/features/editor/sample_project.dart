a663031a3a3505aee6f941a6d1da778645239abb
Leon Moll <lemo@netlight.com>
2026-07-22 10:32:20 +0200

    chore (dartpad-preview-ui): add license headers

--- pkgs/dartpad_preview/dartpad_preview_ui/lib/features/editor/sample_project.dart
+++ pkgs/dartpad_preview/dartpad_preview_ui/lib/features/editor/sample_project.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 const String sampleMainDart = r'''import 'package:flutter/material.dart';
 
 void main() {
