ebb432d0878452f74293fef0f7250da1a8a10111
Kilian Schulte <netlight@schultek.dev>
2026-08-03 12:30:22 +0200

    fix test execution

--- pkgs/dartpad_preview/dartpad_frontend/test/features/filetree/file_tree_view_model_test.dart
+++ pkgs/dartpad_preview/dartpad_frontend/test/features/filetree/file_tree_view_model_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@TestOn('browser')
+library;
+
 import 'dart:async';
 import 'dart:typed_data';
 
