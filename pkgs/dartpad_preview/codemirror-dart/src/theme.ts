4175a0cfb9a14bd6703b7d426e287d8791412502
Kilian Schulte <netlight@schultek.dev>
2026-08-07 10:09:34 +0200

    apply review

--- pkgs/dartpad_preview/codemirror-dart/src/theme.ts
+++ pkgs/dartpad_preview/codemirror-dart/src/theme.ts
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import { EditorView } from "@codemirror/view";
 import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
 import { tags } from "@lezer/highlight";
