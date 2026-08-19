d6e45a724a47abf2322efa4d16e2dfc0ecc914e5
Leon Moll <lemo@netlight.com>
2026-08-19 15:00:57 +0200

    feat(dartpad-preview) add all examples from old dartpad

---
+++ pkgs/dartpad_preview/samples/lib/projects/hello_world/lib/main.dart
@@ -0,0 +1,9 @@
+// Copyright 2015 the Dart project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
+void main() {
+  for (var i = 0; i < 10; i++) {
+    print('hello ${i + 1}');
+  }
+}
