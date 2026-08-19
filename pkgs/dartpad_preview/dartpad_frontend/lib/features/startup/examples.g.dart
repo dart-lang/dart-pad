97123a98316a73218fb521949bc7719831d44aad
Leon Moll <lemo@netlight.com>
2026-08-19 16:01:45 +0200

    chore: fix missing trailing newline in samples.g.dart generator

--- pkgs/dartpad_preview/dartpad_frontend/lib/features/startup/examples.g.dart
+++ pkgs/dartpad_preview/dartpad_frontend/lib/features/startup/examples.g.dart
@@ -15,21 +15,21 @@ abstract final class Examples {
   ];
 
   static const List<Example> samples = [
-    _counter,
     _fibonacci,
-    _flameGame,
     _helloWorld,
+    _counter,
     _sunflower,
+    _flameGame,
   ];
 
   static const List<Example> all = [
+    _fibonacci,
+    _helloWorld,
+    _counter,
+    _sunflower,
+    _flameGame,
     _dart,
     _flutter,
-    _counter,
-    _fibonacci,
-    _flameGame,
-    _helloWorld,
-    _sunflower,
   ];
 
   static Example? getById(String id) {
@@ -44,51 +44,57 @@ abstract final class Examples {
   static const Example defaultExample = _counter;
 }
 
-const _dart = Example(
-  name: 'Dart snippet',
-  id: 'dart',
-  archivePath: 'examples/dart.tar.gz',
-  entryPath: 'lib/main.dart',
-);
-
-const _flutter = Example(
-  name: 'Flutter snippet',
-  id: 'flutter',
-  archivePath: 'examples/flutter.tar.gz',
-  entryPath: 'lib/main.dart',
-);
-
-const _counter = Example(
-  name: 'Counter',
-  id: 'counter',
-  archivePath: 'examples/counter.tar.gz',
-  entryPath: 'lib/main.dart',
-);
-
 const _fibonacci = Example(
   name: 'Fibonacci',
   id: 'fibonacci',
+  subcategory: 'Dart',
+  icon: 'images/dart_logo_192.png',
   archivePath: 'examples/fibonacci.tar.gz',
   entryPath: 'lib/main.dart',
 );
-
-const _flameGame = Example(
-  name: 'Flame game',
-  id: 'flame-game',
-  archivePath: 'examples/flame-game.tar.gz',
-  entryPath: 'lib/main.dart',
-);
-
 const _helloWorld = Example(
   name: 'Hello world',
   id: 'hello-world',
+  subcategory: 'Dart',
+  icon: 'images/dart_logo_192.png',
   archivePath: 'examples/hello-world.tar.gz',
   entryPath: 'lib/main.dart',
 );
-
+const _counter = Example(
+  name: 'Counter',
+  id: 'counter',
+  subcategory: 'Flutter',
+  icon: 'images/flutter_logo_192.png',
+  archivePath: 'examples/counter.tar.gz',
+  entryPath: 'lib/main.dart',
+);
 const _sunflower = Example(
   name: 'Sunflower',
   id: 'sunflower',
+  subcategory: 'Flutter',
+  icon: 'images/flutter_logo_192.png',
   archivePath: 'examples/sunflower.tar.gz',
+  entryPath: 'lib/main.dart',
+);
+const _flameGame = Example(
+  name: 'Flame game',
+  id: 'flame-game',
+  subcategory: 'Ecosystem',
+  icon: 'images/flame_logo_192.png',
+  archivePath: 'examples/flame-game.tar.gz',
+  entryPath: 'lib/main.dart',
+);
+const _dart = Example(
+  name: 'Dart snippet',
+  id: 'dart',
+  icon: 'images/dart_logo_192.png',
+  archivePath: 'examples/dart.tar.gz',
+  entryPath: 'lib/main.dart',
+);
+const _flutter = Example(
+  name: 'Flutter snippet',
+  id: 'flutter',
+  icon: 'images/flutter_logo_192.png',
+  archivePath: 'examples/flutter.tar.gz',
   entryPath: 'lib/main.dart',
 );
